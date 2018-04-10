aca84f404b47e12c2760992f3a66a0fe1bdbfcfa
root <root@DESKTOP-9M7JD9P.localdomain>
2018-04-09 21:36:03 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <Initialize/>
+    <ConversationContainer
+      v-for="id in convoIds"
+      :conversation="conversations[id]"
+      :id="id"
+      :key="id"
+      />
+  </div>
+</template>
+
+<!--script src="https://www.gstatic.com/firebasejs/4.12.1/firebase.js"></script-->
+
+<script>
+import Initialize from './Initialize.vue'
+import ConversationContainer from './ConversationContainer.vue'
+
+import { mapState } from 'vuex'
+
+export default {
+  name: 'app',
+  
+  components: {
+    Initialize,
+    ConversationContainer,
+  },
+
+  computed: {
+    ...mapState({
+      conversations: state => state.conversations.all,
+      convoIds: state => state.conversations.allIds
+    })
+  },
+
+};
+
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/config.js
@@ -0,0 +1,11 @@
+  // Initialize Firebase
+  const config = {
+    apiKey: "AIzaSyBU1ZWBID-e2TlBO_OBntwgmsEr6R68C8M",
+    authDomain: "webappvuechat.firebaseapp.com",
+    databaseURL: "https://webappvuechat.firebaseio.com",
+    projectId: "webappvuechat",
+    storageBucket: "webappvuechat.appspot.com",
+    messagingSenderId: "52216370640"
+  };
+
+  export default config
